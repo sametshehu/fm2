a8af97cafb97dd3f48d5c60c2424572de392d561
sametshehu <samet.shehu@me.com>
2024-10-08 13:07:11 +0200

    script.js aktualisieren

---
+++ script.js
@@ -0,0 +1,44 @@
+// Firebase-Konfiguration
+const firebaseConfig = {
+  apiKey: "AIzaSyD5asuJN_0f_wcP3KidUqAYyEsCvtPaIeI",
+  authDomain: "mitgliederverwaltung-3458b.firebaseapp.com",
+  projectId: "mitgliederverwaltung-3458b",
+  storageBucket: "mitgliederverwaltung-3458b.appspot.com",
+  messagingSenderId: "916656812587",
+  appId: "1:916656812587:web:23b510f805c800f62af06e"
+};
+
+// Firebase initialisieren
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const membersCollection = db.collection("members");
+const financeCollection = db.collection("finances");
+
+document.getElementById("openAddModalButton").addEventListener("click", () => {
+    document.getElementById("addMemberModal").style.display = "block";
+});
+
+document.getElementById("closeAddModal").addEventListener("click", () => {
+    document.getElementById("addMemberModal").style.display = "none";
+});
+
+// Handle member form submission
+document.getElementById("addMemberForm").addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const member = {
+        memberNumber: document.getElementById("addMemberNumber").value,
+        name: document.getElementById("addName").value,
+        email: document.getElementById("addEmail").value,
+        phone: document.getElementById("addPhone").value,
+        address: document.getElementById("addAddress").value,
+        zip: document.getElementById("addZip").value,
+        city: document.getElementById("addCity").value
+    };
+    try {
+        await membersCollection.add(member);
+        document.getElementById("addMemberForm").reset();
+        document.getElementById("addMemberModal").style.display = "none";
+    } catch (error) {
+        console.error("Fehler beim Hinzufügen des Mitglieds: ", error);
+    }
+});
