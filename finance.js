aa9a367ebd66fd4dc72646d47ced110e2f28be72
sametshehu <samet.shehu@me.com>
2024-10-08 13:19:12 +0200

    Update finance.js

--- finance.js
+++ finance.js
@@ -1,11 +1,22 @@
+// Firebase-Konfiguration
+const firebaseConfig = {
+    apiKey: "AIzaSyD5asuJN_0f_wcP3KidUqAYyEsCvtPaIeI",
+    authDomain: "mitgliederverwaltung-3458b.firebaseapp.com",
+    projectId: "mitgliederverwaltung-3458b",
+    storageBucket: "mitgliederverwaltung-3458b.appspot.com",
+    messagingSenderId: "916656812587",
+    appId: "1:916656812587:web:23b510f805c800f62af06e"
+};
+
 // Firebase initialisieren
 firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
 const financeCollection = db.collection("finances");
 
+// Modal öffnen/schließen
 document.getElementById("openAddFinanceModalButton").addEventListener("click", () => {
     document.getElementById("addFinanceModal").style.display = "block";
 });
-
 document.getElementById("closeAddFinanceModal").addEventListener("click", () => {
     document.getElementById("addFinanceModal").style.display = "none";
 });
@@ -30,3 +41,4 @@ document.getElementById("addFinanceForm").addEventListener("submit", async (e) =
         console.error("Fehler beim Hinzufügen des Finanzdatensatzes: ", error);
     }
 });
+v
